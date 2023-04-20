fd96d428f667a131f41a55f0b053391dc9f5e100
fabio morales <fabio.l.morales@hotmail.com>
2023-04-20 18:05:08 -0300

    refact: ajusta rota na pagina NoMatch

--- src/components/pages/NoMatch/index.tsx
+++ src/components/pages/NoMatch/index.tsx
@@ -1,12 +1,13 @@
 import { FC } from 'react';
 import { Link } from 'react-router-dom';
+import { routerPaths } from '../../../utils/routes';
 
 const NoMatch: FC = () => {
   return (
     <div>
       <h2>Nothing to see here!</h2>
       <p>
-        <Link to="/">Go to the home page</Link>
+        <Link to={routerPaths.home.default}>Go to the home page</Link>
       </p>
     </div>
   );
